5b07a600350e8e451821dabea29f16ed0dbdb1e7
shininglite <vandeusent@gmail.com>
2020-05-07 16:40:17 -0500

    add route for book/id:

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/book');
 
-const book = require("../models/book.js");
+router.get('/api/books', (req, res) => {
+  Book.getAllBooks()
+  .then(results => res.json(results))
+  .catch(error => res.json(error))
+});
 
-router.get('/api/books/', (req, res) => {
-    book.getAllBooks()
-        .then(results => res.json(results))
-        .catch(error => res.json(error))
+router.get('/api/book/:id', (req, res) => {
+  const bookId = req.params.id;
+  Book.getOneBook(bookId)
+  .then(results => res.json(results))
+  .catch(error => res.json(error))
 })
 
 router.post('/api/book/new', (req, res) => {
-    const { title, coverPhoto, authorId } = req.body;
-    book.addBook(title, coverPhoto, authorId)
-        .then(() => res.status(200).json(true))
-        .catch(error => res.status(500).json(error))
-});
+  const { title, coverPhoto, authorId } = req.body;
+
+  Book.addBook([title, coverPhoto, authorId])
+  .then(() => res.status(200).json(true))
+  .catch(error => res.status(500).json(error))
+})
 
 module.exports = router;
