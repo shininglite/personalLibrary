c736ba57c8c989720f827d0157bf97ddfc17dced
shininglite <vandeusent@gmail.com>
2020-05-07 18:11:23 -0500

    redirect server to use controllers folder javascript files for books, notes and html

--- server.js
+++ server.js
@@ -7,15 +7,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/htmlRoutes')(app);
-
-
-const bookRoutes = require("./controllers/booksController.js");
-// const noteRoutes = require("./controllers/notesController.js");
-
-app.use(bookRoutes);
-// app.use(noteRoutes);
-
+app.use(require('./controllers/booksController.js'));
+app.use(require('./controllers/notesController.js'));
+app.use(require('./controllers/htmlController.js'));
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`)
 });
