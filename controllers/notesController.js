6ec6ff267570802de578aa879e72c72bd9ef3311
shininglite <vandeusent@gmail.com>
2020-05-07 16:50:31 -0500

    change lower case note to initial caps Note in all code blocks

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Note = require('../models/note');
 
-const note = require("../models/note.js");
+router.get('/api/book/notes/:id', (req, res) => {
+  const bookId = req.params.id;
 
-router.get("/api/book/notes/:bookTitle", (req, res) => {
-    const bookTitle = req.params.bookTitle
-    note.getBookNotes(bookTitle)
-    .then(results => res.json(results))
-    .catch(error => res.status(500).json(error))
+  Note.getBookNotes(bookId)
+  .then(results => res.json(results))
+  .catch(error => {
+    console.log(error)
+    res.status(500).json(error)
+  })
 });
 
 router.post('/api/book/note', (req, res) => {
-    const { note, bookId } = req.body;
-    note.addBookNote(note, bookId)
-    .then(() => res.status(200).json(true))
-    .catch(error => res.status(500).json(error))
-})
+  const { note, bookId } = req.body;
+  
+  Note.addBookNote([note, bookId])
+  .then(() => res.status(200).json(true))
+  .catch(error => res.status(500).json(error))
+});
 
 router.delete('/api/note/:id', (req, res) => {
-    note.deleteNote(req.params.id)
-    .then(() => res.status(200).json(true))
-    .catch(error => res.status(500).json(error))
-})
+  Note.deleteBookNote(req.params.id)
+  .then(() => res.status(200).json(true))
+  .catch(error => res.status(500).json(error))
+});
 
 module.exports = router;
