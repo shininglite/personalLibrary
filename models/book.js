d197f5ef11d24474a04dafb099f7dc6a09a9b607
shininglite <vandeusent@gmail.com>
2020-05-07 16:59:46 -0500

    add comments to book.js

--- models/book.js
+++ models/book.js
@@ -1,15 +1,17 @@
+// Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
 class Book {
-    getAllBooks() {
-        return orm.getAllBooksORM()
-    }
-    getOneBook(bookTitle) {
-        return orm.getOneBookORM(bookTitle);
-    }
-    addBook(title, coverPhoto, authorId) {
-        return orm.addBookORM(title, coverPhoto, authorId)
-    }
-}
+  getAllBooks() {
+    return orm.innerJoin(['books.id', 'firstName', 'lastName', 'title', 'coverPhoto'], 'authors', 'books', 'id', 'authorId')
+  }
+  getOneBook(bookId){
+    return orm.innerJoinOne(['books.id', 'firstName', 'lastName', 'title', 'coverPhoto'], 'authors', 'books', 'id', 'authorId', bookId)
+  }
+  addBook(values) {
+    return orm.create("books", ['title', 'coverPhoto', 'authorId'], values)
+  }
+};
 
+// Export the database functions for the controller.
 module.exports = new Book();
