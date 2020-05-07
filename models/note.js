ccd59d4616ef26e5c9bd223224e1e75da4526d99
shininglite <vandeusent@gmail.com>
2020-05-07 17:23:31 -0500

    define join between a book and its notes, create add and delete methods for notes

--- models/note.js
+++ models/note.js
@@ -1,15 +1,15 @@
 const orm = require("../config/orm.js");
 
 class Note {
-    getBookNotes(bookTitle){
-        return orm.getBookNotesORM(bookTitle);
-    }
-    addBookNotes(note, bookId){
-        return orm.addBookNotesORM(note, bookId);
-    }
-    deleteBookNote(noteId){
-        return orm.deleteNoteORM(noteId);
-    }
-}
+  getBookNotes(bookId) {
+    return orm.innerJoinOne(['notes.id', 'title', 'note'], 'books', 'notes', 'id', 'bookId', bookId)
+  }
+  addBookNote(values) {
+    return orm.create("notes", ['note', 'bookId'], values)
+  }
+  deleteBookNote(value) {
+    return orm.delete("notes", 'id', value)
+  }
+};
 
 module.exports = new Note();
