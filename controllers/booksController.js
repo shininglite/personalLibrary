b0173bba1bda8c1a70b806713b4df27725004463
shininglite <vandeusent@gmail.com>
2020-05-07 16:38:44 -0500

    removed api/book/name route

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -9,13 +9,6 @@ router.get('/api/books/', (req, res) => {
         .catch(error => res.json(error))
 })
 
-router.get('/api/book/:name', (req, res) => {
-    const bookName = req.params.name;
-    book.getOneBook(bookName)
-        .then(results => res.json(results))
-        .catch(error => res.json(error))
-})
-
 router.post('/api/book/new', (req, res) => {
     const { title, coverPhoto, authorId } = req.body;
     book.addBook(title, coverPhoto, authorId)
